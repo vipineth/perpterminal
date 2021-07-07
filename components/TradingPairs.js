320dad9a5048aa2aea2160f73cddbc14a022d0b7
Vipin <vipineth@gmail.com>
2021-07-07 19:58:37 +0530

    add more stats on homepage

--- components/TradingPairs.js
+++ components/TradingPairs.js
@@ -48,9 +48,9 @@ function TBody(props) {
             <Link href={`/asset/${props.symbol.toLowerCase()}`}>
               <a
                 type="button"
-                className="inline-flex items-center px-4 py-2 border-transparent text-sm font-medium text-green-800"
+                className="inline-flex items-center px-4 py-2 rounded-md border-transparent bg-green-600 text-sm font-medium text-white hover:bg-green-800"
               >
-                Know More
+                More Stats
                 <ArrowSmRightIcon className="ml-2 h-5 w-5" aria-hidden="true" />
               </a>
             </Link>
