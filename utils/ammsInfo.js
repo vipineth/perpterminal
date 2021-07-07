68929d03b6c1b680fb00c1a9ddae85124d3a8d7a
Vipin <vipineth@gmail.com>
2021-07-07 17:42:05 +0530

    add token names

--- utils/ammsInfo.js
+++ utils/ammsInfo.js
@@ -1,26 +1,31 @@
 export default {
   BTC: {
     symbol: "BTC",
+    name: "Bitcoin",
     logoURI:
       "https://github.com/trustwallet/assets/blob/master/blockchains/bitcoin/info/logo.png?raw=true",
   },
   ETH: {
     symbol: "ETH",
+    name: "Ethereum",
     logoURI:
       "https://github.com/trustwallet/assets/blob/master/blockchains/ethereum/info/logo.png?raw=true",
   },
   DOT: {
     symbol: "DOT",
+    name: "Polkadot",
     logoURI:
       "https://github.com/trustwallet/assets/blob/master/blockchains/polkadot/info/logo.png?raw=true",
   },
   ALPHA: {
+    name: "Alpha Finance",
     symbol: "ALPHA",
     logoURI:
       "https://assets.coingecko.com/coins/images/12738/small/AlphaToken_256x256.png",
   },
   FTT: {
     symbol: "FTT",
+    name: "FTX Token",
     logoURI: "https://assets.coingecko.com/coins/images/9026/small/F.png",
   },
 };
