022cd4f459952a281fc0ba1621359995704e1ffb
Vipin <vipineth@gmail.com>
2021-10-13 15:38:33 +0530

    add favicon image

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
 import Footer from "./Footer";
+import Head from "next/head";
 
 function Layout(props) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {props.children}
       <Footer />
     </>
